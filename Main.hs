2b7a014aad44beb78a0ac540c22c337c3481218f
Kokan <kokaipeter@gmail.com>
2021-01-17 19:18:20 +0100

    new example program2 /using variables/
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- Main.hs
+++ Main.hs
@@ -13,7 +13,13 @@ program = [PushInteger 1, PushFunction [Dup, PrintNum, Dup, PushInteger 1, Add],
 program1 :: Commands
 program1 = [PrintStr "start=", Flush, ReadCh, PushFunction [Dup, PrintNum, Dup, PushInteger 1, Add], PushFunction [Dup, PushInteger 10, Larger], While]
 
+program2 :: Commands
+program2 = [PushFunction [PrintStr "start=", Flush, ReadCh], PushVaradr 'q', AssignVar,
+            PushFunction [Dup, PrintNum, Dup, PushInteger 1, Add], PushVaradr 'b', AssignVar,
+            PushFunction [Dup, PushInteger 10, Larger], PushVaradr 'c', AssignVar,
+            PushFunction [PushVaradr 'q', PushVar, RunFunction, PushVaradr 'b', PushVar, PushVaradr 'c', PushVar, While], PushVaradr 'm', AssignVar,
+            PushVaradr 'm', PushVar, RunFunction]
 
 --main :: Prelude.IO ()
-main = runStateT (execute program1) emptyUniverse
+main = runStateT (execute program2) emptyUniverse
 
