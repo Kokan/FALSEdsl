78b618e152b240662154a2a16d5e95d10d3bd48b
Kokan <kokaipeter@gmail.com>
2021-01-17 17:50:55 +0100

    add new program1 example
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- Main.hs
+++ Main.hs
@@ -8,10 +8,12 @@ import ADT
 import Interpreter
 
 program :: Commands
-program = [PushInteger 1, PushFunction [Dup, PushInteger 1, Add], PushFunction [Dup, PushInteger 10, Larger], While]
+program = [PushInteger 1, PushFunction [Dup, PrintNum, Dup, PushInteger 1, Add], PushFunction [Dup, PushInteger 10, Larger], While]
 
+program1 :: Commands
+program1 = [PrintStr "start=", Flush, ReadCh, PushFunction [Dup, PrintNum, Dup, PushInteger 1, Add], PushFunction [Dup, PushInteger 10, Larger], While]
 
 
 --main :: Prelude.IO ()
-main = runStateT (execute program) []
+main = runStateT (execute program1) []
 
