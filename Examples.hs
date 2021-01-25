c875219b6aa22b46e97663b519ec3fbadc9d5217
Kokan <kokaipeter@gmail.com>
2021-01-25 16:29:47 +0100

    finalize examples
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- Examples.hs
+++ Examples.hs
@@ -6,38 +6,38 @@ import AbstractSyntax
 import AbstractSyntaxTypeClasses
 import False
 
+-- 1a:"please provide a number: "ø^a;+,
 dummy :: Commands
-dummy = [0, a, (\:), "please provide a number: ", ab, (^), a, sc, (\\), (-),  (.)]
+dummy = [1, a, (\:), "please provide a char: ", ab, (^), a, sc, (+),  cc]
 
-program :: Commands
-program = [1, [($), 10, (\\), (>)], [($), (.), ($), 1, (+)], (#)]
+-- 1[$10\>][$.$1+]#
+print1to9 :: Commands
+print1to9 = [1, [($), 10, (\\), (>)], [($), (.), ($), 1, (+)], (#)]
 
-larger :: Commands
-larger = [ 3, 2, (>), (.) ]
-
-program1 :: Commands
-program1 = ["start=", ab, 48, (^), (\\), (-), [($), 10, (\\), (>)], [($), (.), ($), 1, (+)], (#)]
+-- "start="ß48^\-[$10\>][$.$1+]#
+printnto9 :: Commands
+printnto9 = ["start=", ab, 48, (^), (\\), (-), [($), 10, (\\), (>)], [($), (.), ($), 1, (+)], (#)]
 
 --   ß[^$1_=~][,]#		{ while c:=getc()<>EOF do putc(c) }
 cp :: Commands
 cp = [ ab, [ (^), ($), 1, mm, (==), (\~) ], [ cc ], (#) ]
 
-program2 :: Commands
-program2 = [["start=", ab, 48, (^), (\\), (-)], q, (\:),
+print1to9_1 :: Commands
+print1to9_1 = [["start=", ab, 48, (^), (\\), (-)], q, (\:),
             [($), (.), ($), 1, (+)], b, (\:),
             [($), 10, (\\), (>)], c, (\:),
             [q, sc, (!), c, sc, b, sc, (#)], m, (\:),
             m, sc, (!)]
 
-program3 :: Commands
-program3 = [[["start=", ab, 48, (^), (\\), (-)], q, (\:),
+print1to9_2 :: Commands
+print1to9_2 = [[["start=", ab, 48, (^), (\\), (-)], q, (\:),
             [($), (.), ($), 1, (+)], b, (\:),
             [($), 10, (\\), (>)], c, (\:),
             [q, sc, (!), c, sc, b, sc, (#)], m, (\:),
             m, sc, (!)], (!)]
 
--- List primes up to 99
--- 99 9[1-$][\$@$@$@$@\/*=[1-$$[%\1-$@]?0=[\$.' ,\]?]?]#
+-- List primes up to 1000
+-- 1000 9[1-$][\$@$@$@$@\/*=[1-$$[%\1-$@]?0=[\$.' ,\]?]?]#
 primes :: Commands
 primes = [ 1000, 9,
            [ 1, (-), ($) ],
@@ -51,6 +51,25 @@ factorial :: Commands
 factorial = [ [ ($), 1, (==), ($), [ (\\), (%), 1, (\\) ], (?), (\~), [ ($), 1, (-), f, sc, (!), (*) ], (?) ], f, (\:),
               5, f, sc, (!), (.) ]
 
+--{ faculty program in false! }
+-- 
+-- [$1=$[\%1\]?~[$1-f;!*]?]f:          { fac() in false }
+-- 
+-- "calculate the faculty of [1..8]: "
+-- ab^ab'0-$$0>~\8>|$
+-- "result: "
+-- ~[\f;!.]?
+-- ["illegal input!"]?"
+-- "
+
+fac :: Commands
+fac = [ [($), 1, (==), ($), [ (\\), (%), 1, (\\) ], (?), (\~), [ ($), 1, (-), f, sc, (!), (*) ], (?) ], f, (\:),
+        "calculate the faculty of [1..8]: ",
+        ab, (^), ab, PushChar '0', (-), ($), ($), 0, (>), (\~), (\\), 8, (>), (\|), ($),
+        "result ",
+        (\~), [ (\\), f, sc, (!), (.) ], (?),
+        ["illegal input"], (?), 10, cc ]
+
 -- tafels: 0[1+$11\>][0[1+$11\>][1ø."x"$."="$2ø*." "]#10,%]#
 tafels :: Commands
 tafels = [ 0,
@@ -62,8 +81,12 @@ tafels = [ 0,
                           10,  cc,  (%)],
            (#) ]
 
-pickTest :: Commands
-pickTest = [ 3, 2, 1, 4, ao, (.) ]
+
+--{ False version of 99 Bottles by Marcus Comstedt (marcus@lysator.liu.se) }
+--[$0=["no more bottles"]?$1=["One bottle"]?$1>[$." bottles"]?%" of beer"]b:
+--100[$0>][$b;!" on the wall, "$b;!".
+--"1-"Take one down, pass it around, "$b;!" on the wall.
+--"]#%
 
 beer :: Commands
 beer = [ 
@@ -76,37 +99,6 @@ beer = [
                  (?),  (%),  " of beer"],
    b,  (\:) ,  100,
    [ ($),  0,  (>)],
-   [ ($),  b,  sc,  (!),  " on the wall, ",  ($),  b,  sc,  (!),  ".\\n",  1,  (-),  "Take one down, pass it around, ",  ($),  b,  sc,  (!),  " on the wall.\\n"],  (#),  (%) ]
+   [ ($),  b,  sc,  (!),  " on the wall, ",  ($),  b,  sc,  (!),  ".", 10, cc,  1,  (-),  "Take one down, pass it around, ",  ($),  b,  sc,  (!),  " on the wall.", 10, cc],  (#),  (%) ]
 
--- [[$' =][%^]#]b:
--- [$$'.=\' =|~]w:
--- [$'.=~[' ,]?]s:
--- [w;![^o;!\,]?]o:
--- ^b;![$'.=~][w;[,^]#b;!s;!o;!b;!s;!]#,
-oddword :: Commands
-oddword = [  [ [ ($), PushChar ' ',  (==)],  [ (%),  cc],  (#)],  b,  (\:),
-             [ ($),  ($), PushChar ' ',  (.),  (==),  (\\), PushChar ' ',  (==),  Or,  (\~)],  w,  (\:),
-             [ ($), PushChar ' ',  (.),  (==),  (\~),  [ PushChar ' ',  cc],  (?)],  s,  (\:),
-             [ w,  sc,  (!),  [ cc,  o,  sc,  (!),  (\\),  cc],  (?)],  o,  (\:),
-             (^), b,  sc,  (!),  [ ($), PushChar ' ',  (.),  (==),  (\~)],  [ w,  sc,  [ (^),  cc],  (#),  b,  sc,  (!),  s,  sc,  (!),  o,  sc,  (!),  b,  sc,  (!),  s,  sc,  (!)],  (#),  cc ]
-
---{ faculty program in false! }
--- 
--- [$1=$[\%1\]?~[$1-f;!*]?]f:          { fac() in false }
--- 
--- "calculate the faculty of [1..8]: "
--- ab^ab'0-$$0>~\8>|$
--- "result: "
--- ~[\f;!.]?
--- ["illegal input!"]?"
--- "
-
-
-fac :: Commands
-fac = [ [($), 1, (==), ($), [ (\\), (%), 1, (\\) ], (?), (\~), [ ($), 1, (-), f, sc, (!), (*) ], (?) ], f, (\:),
-        "calculate the faculty of [1..8]: ",
-        ab, (^), ab, PushChar '0', (-), ($), ($), 0, (>), (\~), (\\), 8, (>), (\|), ($),
-        "result ",
-        (\~), [ (\\), f, sc, (!), (.) ], (?),
-        ["illegal input"], (?), 10, cc ]
 
